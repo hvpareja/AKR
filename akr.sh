969e962251424891c3246924a52f9270e0d04c41
Héctor Valverde <hvpareja@hotmail.com>
2012-08-10 20:34:35 +0200

    Sequence and counting

--- akr.sh
+++ akr.sh
@@ -1,4 +1,9 @@
 inputpdbfile=$1
-./dssp $inputpdbfile dssp_temp
-./sequences.pl
+n=$2
+./dssp $inputpdbfile dssp_temp 2> /dev/null
+./sequences.pl prot_$n > temp_seq 2> /dev/null
+grep "^[^>]" temp_seq | ./resCounter.pl > temp_countRes
+
+rm temp_countRes
 rm dssp_temp
+rm temp_seq
--- sequences.pl
+++ sequences.pl
@@ -1,37 +1,13 @@
-# Author information
+#!/usr/bin/perl -w
+# Author and script information
 
 use warnings;
 use Bio::Structure::Model;
 use Bio::Structure::IO::pdb;
 use Bio::Structure::SecStr::DSSP::Res;
 
-# ASA calculated for residue X in a GXG tripeptide with the main chain in an
-# extended conformation. This hash information is necessary to calculate
-# the relative exposition of each type of residue.
-my %ASA_GXG_total =  (
-   'A' => 113,
-   'R' => 241,
-   'N' => 158,
-   'D' => 151,
-   'C' => 140,
-   'Q' => 189,
-   'E' => 183,
-   'G' => 85,
-   'H' => 194,
-   'I' => 182,
-   'L' => 180,
-   'K' => 211,
-   'M' => 204,
-   'F' => 218,
-   'P' => 143,
-   'S' => 122,
-   'T' => 146,
-   'W' => 259,
-   'Y' => 229,
-   'V' => 160,
-# X: ambigueties
-   'X' => 90
- );
+# Getting external parameters
+my $protein_name = $ARGV[0];
 
 # DSSP processor
 #  GETTING DSSP INFORMATION
@@ -43,3 +19,21 @@ my $dssp = new Bio::Structure::SecStr::DSSP::Res('-file' => "dssp_temp" );
 
 #  Store the whole information about each residue
 my @residues = $dssp->residues();
+
+# Print the fasta item head
+print ">$protein_name\n";
+
+# Print the sequence from the coordinates file
+for my $resID (@residues){
+        
+    # Print one residue
+    my $res = $dssp->resAA($resID);
+    # Sometimes, Cys resisues are designated wiht the characters 'a' or 'b'
+    # when they are involved in disulfure bridges
+    if ($res eq 'a' or $res eq 'b'){ $res = 'C'; }
+    
+    print $res;
+    
+}
+
+print "\n";
